c454543734c19f1ced478d38c289f8b2950c7233
BJKill <brandon.j.kill@gmail.com>
2020-12-03 15:22:55 -0500

    added if statements to check for valid input parameters to the functions.

--- FinalProject.R
+++ FinalProject.R
@@ -11,8 +11,8 @@ library(dplyr)
 library(stringr)
 
 make_model_matrix <- function(n,p) {
+        if (n <= 0 | p <= 0 | is.integer(n) = F | is.integer(p) = F) {return("n and p must be positive integers")}
         X <- matrix(nrow = n, ncol = p)
-        #X[ ,1] <- rep(1,n)
         for (i in 1:p) {
                 X[ ,i] <- rnorm(n)
         }
@@ -21,6 +21,7 @@ make_model_matrix <- function(n,p) {
 
 
 make_response_vector <- function(X, k) {
+        if (k > ncol(X) | is.integer(k) = F) {return("k must be a positive integer not exceeding p")}
         Y <- vector(length = nrow(X))
         for (i in 1:nrow(X)) {
                 Y[i] <- rnorm(1)
@@ -42,6 +43,7 @@ make_data_frame <- function(n,p,k) {
 
 run_BE <- function(n,p,k,alpha) {
         df <- make_data_frame(n,p,k)
+        if (alpha < 0 | alpha > 1) {return("alpha must be in the interval (0,1)")}
         while(summary(lm(Y~.,df))$coefficients[1+which.max(summary(lm(Y~., df))$coefficients[-1,4]),4] > alpha) {
                 rem_inx <- which.max(summary(lm(Y~., df))$coefficients[-1,4])
                 df <- df[,-rem_inx]
@@ -63,22 +65,20 @@ final
 
 #debug(run_BE)
 
-run_simulation <- function(n,p,k,aplha,m) {
+run_simulation <- function(n,p,k,alpha,m) {
+        if (m <= 0 | is.integer(m) = F) {return("m must be a positive integer")}
         CI_freq <- vector(length = p+1)
-        #display_array <- array(dim = c(n,p,m))
-        #rownames(CI_freq) <- rownames(summ)
+        full_df <- make_data_frame(n,p,k)
+        names(CI_freq) <- rownames(summary(lm(Y~.,full_df))$coefficients)
         for (i in 1:m) {
                 display <- run_BE(n,p,k,alpha)
                 CI_freq[1] <- CI_freq[1] + display[1,7]
                 for (j in 2:nrow(display)) {
                         CI_freq[as.numeric(str_sub(rownames(display)[j], 2,-1))+1] <- CI_freq[as.numeric(str_sub(rownames(display)[j], 2,-1))+1] + display[j,7]
                 }
-                #display_array[,,i] <- display
         }
-        #print(display_array[,,1])
-        #print(display_array[,,2])
         return(CI_freq / m)
 }
 
-
+run_simulation(100,50,10,0.05,1000)
 
