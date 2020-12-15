1368d048abe0a5c20b06a76004ace39d4c27a9f4
BJKill <brandon.j.kill@gmail.com>
2020-12-15 14:00:18 -0500

    Made one test to check dimensions of df. Need to figure out what I'm missing to make it work right.

--- FinalProjectTests.R
+++ FinalProjectTests.R
@@ -1,11 +1,16 @@
 #### Final Project Tests
 
 library(testthat)
+test_dir(".")
 
+context("Check dimensions of generated data frame")
+source("./FinalProject.R")
 
-
-
-
+test_that("DF dimension are exactly n x (p+1)", {
+        expect_equal(dim(make_data_frame(20,10,5)), c(20,11))
+        expect_equal(dim(make_data_frame(50,20,10)), c(50,21))
+        expect_equal(dim(make_data_frame(3,1,1)), c(3,2))
+})
 
 
 
